2d0492611fc1cc8ef7a310f4dfcd77820b4591c8
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2016-11-23 17:10:19 +0000

    fix run.py to use port 80

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+import os
 from app import app
 from app import scrape_4_dead_links
 
 execute_checks = scrape_4_dead_links.run_check() 
 
+#app.debug = True
+port = int(os.environ.get("PORT", 5000))
 app.run( 
         host="0.0.0.0",
-        port=int("80")
+        port=port
 )
