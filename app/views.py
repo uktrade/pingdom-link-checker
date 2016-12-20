342de18e6f78612e120c75f5946c94a20f5f26c3
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2016-12-20 10:53:19 +0000

    Pingdom XML now shows failed links

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from app import app, send_from_directory
+from app import app, send_from_directory, scrape_4_dead_links
 import os
 
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
@@ -10,3 +10,8 @@ def serve_static_path(my_path):
 @app.route('/', methods=['GET'])
 def serve_static_check():
 	return send_from_directory(root, "check.xml")
+
+@app.route('/refresh')
+def refresh_check():
+	execute_checks = scrape_4_dead_links.run_check()
+	return send_from_directory(root, "refresh.html")
