a50f99007b7425819be58a7854e93e420b5676d2
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2018-03-05 14:14:38 +0000

    refactor code with additional comments

--- app/scrape_4_dead_links.py
+++ app/scrape_4_dead_links.py
@@ -1,6 +1,4 @@
 import threading, subprocess, os, time, json
-from subprocess import call
-from collections import namedtuple, defaultdict
 import datetime
 import commands
 
@@ -9,82 +7,93 @@ class run_check(object):
 	def __init__(self):
 
 			self.interval = int(os.environ.get('CHECK_INTERVAL'))
-
 			print "This check will run every %ds"  % self.interval
 
 			thread = threading.Thread(target=self.run, args=())
-			thread.daemon = True                            # Daemonize thread
+			thread.daemon = True                            													# Daemonize thread.
 			thread.start()
 		
 
 	def run(self):
-		""" Method that runs forever """
-		check_log_url = "https://pingdom-link-checker.ukti.io/logs.html"
-
+																												# Method that runs forever.
+		#check_log_url = "https://pingdom-link-checker.ukti.io/logs.html"
+																												# Open file that contains the URLs to check.
 		with open('url_list.json') as json_file:
 				all_urls = json.load(json_file)
-
+																												# Initialize a url list with all green OK status.
 		my_list = {}
-		#import pdb; pdb.set_trace()
 		for key in all_urls.iteritems():
-			for url_found in key[1]:
-				my_list[url_found] = 'OK'		
+			for url_name in key[1]:
+				my_list[url_name] = 'OK'		
 
 		while True:
+																												# Pingdom checks for XML in a set format.
 			xml_out_1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 			xml_out_2 = "<pingdom_http_custom_check>"
 			xml_out_3 = ""
 			xml_out_5 = "</pingdom_http_custom_check>"
-			dead_link_found = 0
+			dead_link_found = False
 			t0 = time.time()
-
-			print datetime.datetime.now()
-
+																												# Create logs.html for storing link check results.
 			with open('app/templates/logs.html','w') as out:
 				out.write ('{}\n{}\n{}\n{}\n'.format('<html>','<body>','<h1>Link check - logs</h1>','<p>'))
-
+																												# For all URLs check for 404 dead links.
 			for current_url, status in my_list.iteritems():
-
-				FourOFour = 0
-				count = 0
+				no_of_four_o_four = 0
+				current_item_pos = 0
 				print "%s" % current_url
+																												# Update log file with url name.
 				with open('app/templates/logs.html','a') as out:
 					out.write ('{}'.format(current_url))
-				o = commands.getoutput('wget --spider -t 3 -e robots=off -r -p ' + current_url)
-				listfiles = o.split('\n')
+																												# Use wgets spider to check all links
+																												# -t (3s) time to wait if link doesnt respond,
+																												# remove check for robots.txt,
+																												# -r recurse website. 
+																												# Output results to a list.
+				output_results = commands.getoutput('wget --spider -t 3 -e robots=off -r -p ' + current_url)
+				list_of_results = output_results.split('\n')
+				
 				dead_link_array = []
-				for item in listfiles:
-					
-					count = count + 1
+				for item in list_of_results:
+																												# Check list for 404, this is found at end of string.
+																												# If found store bad link in array which is 
+																												# 3 positions before the 404 is found.
+					current_item_pos = current_item_pos + 1
 					if item[-13:] == '404 Not Found':
-						FourOFour = FourOFour + 1 
-						print listfiles[count-3]
+						no_of_four_o_four = no_of_four_o_four + 1 
+						print list_of_results[current_item_pos-3]
 						print item
-						dead_link_array.append(listfiles[count-3])
+						dead_link_array.append(list_of_results[current_item_pos-3])
+																												# Store bad link in log file and record that a dead link was found.
 						with open('app/templates/logs.html','a') as out:
-							out.write ('{}{}\n{}{}{}\n'.format(' - FAILED','<br/>','404 - ',listfiles[count-3],'<br/>'))
-						dead_link_found = 1
-
-				if ( (FourOFour > 0) and (status == 'OK')):
+							out.write ('{}{}\n{}{}{}\n'.format(' - FAILED','<br/>','404 - ',list_of_results[current_item_pos-3],'<br/>'))
+						dead_link_found = True
+																												# If a 404 is found and this is the first time this loop has run
+																												# then record the dead link.  This is so that this doesn't
+																												# get overwritten if this dead link was found during the previous run
+																												# and we keep a record of the time that the link was originally found.
+				if ((no_of_four_o_four > 0) and (status == 'OK')):
 					my_list[current_url] = dead_link_array
-
-				if (FourOFour == 0):
+				
+				if (no_of_four_o_four == 0):
 					my_list[current_url] = "OK"
 
-				print datetime.datetime.now()
-				print "Number of Dead Links: %d" %FourOFour
+				print "Number of Dead Links: %d" %no_of_four_o_four
+																												# If no 404s found then mark url as good.
 				with open('app/templates/logs.html','a') as out:
-					if (FourOFour == 0 ):
+					if (no_of_four_o_four == 0 ):
 						out.write ('{}{}\n'.format(' - GOOD', '<br/>'))
-
+																												# Checks done close logs.
 			with open('app/templates/logs.html','a') as out:
 				out.write ('{}\n{}\n{}\n{}\n'.format('--  All Sites Checked --','</p>','</body>','</html>'))
-
-
+																												# Record time it took to run checks so that it can be displayed in 
+																												# pingdoms response time.
 			t1 = time.time()
 			total_time = (t1-t0)*1000
 
-			if ( dead_link_found == 1 ):
+																												# Ouput urls link status to pingdoms check XML,
+																												# this is what pingdom points to.
+			if ( dead_link_found == True ):
 				with open('app/templates/check.xml','w') as out:
 					out.write('{}\n{}\n{}\n'.format(xml_out_1,xml_out_2,"<status>"))
 					
@@ -102,10 +111,11 @@ class run_check(object):
 					xml_out_4 = "<response_time>%.2f</response_time>" % total_time
 					out.write('{}\n{}\n'.format(xml_out_4,xml_out_5))
 
-			if ( dead_link_found == 0 ):
+			if ( dead_link_found == False ):
 				with open('app/templates/check.xml','w') as out:
 					xml_out_3 = "<status>OK</status>"
 					xml_out_4 = "<response_time>%.2f</response_time>" % total_time
 					out.write('{}\n{}\n{}\n{}\n{}\n'.format(xml_out_1,xml_out_2,xml_out_3,xml_out_4,xml_out_5))
+																												# Sleep for duration of check interval.
 			time.sleep(self.interval)
 
