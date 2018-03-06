600297f0f3a23fdf73a039e70d665cff816aa6f2
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2018-03-06 14:00:17 +0000

    Changes from review by Lyndon upgrade to Python 3

--- app/views.py
+++ app/views.py
@@ -1,17 +1,20 @@
-from app import app, send_from_directory, scrape_4_dead_links
+from app import app, scrape_4_dead_links, send_from_directory
 import os
 
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
 
+
 @app.route('/<path:my_path>', methods=['GET'])
 def serve_static_path(my_path):
-	return send_from_directory(root, my_path)
+    return send_from_directory(root, my_path)
+
 
 @app.route('/', methods=['GET'])
 def serve_static_check():
-	return send_from_directory(root, "check.xml")
+    return send_from_directory(root, "check.xml")
+
 
 @app.route('/refresh')
 def refresh_check():
-	execute_checks = scrape_4_dead_links.run_check()
-	return send_from_directory(root, "refresh.html")
+    scrape_4_dead_links.run_check()
+    return send_from_directory(root, "refresh.html")
